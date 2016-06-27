5d869d72206b273987312c69b54d22dacdc853a5
Barnabas Bulpett <barnabas@mwwondemand.com>
2016-06-27 13:29:50 -0400

    Fixes to justification module error handling methods

--- lib/zebra/epl/justification.rb
+++ lib/zebra/epl/justification.rb
@@ -1,21 +1,20 @@
 module Zebra
   module Epl
     module Justification
-      class JustificationError < StandardError; end
+      class InvalidJustificationError < StandardError; end
 
-        LEFT        = "L"
-        CENTER      = "C"
-        RIGHT       = "R"
-        JUSTIFIED   = "J"
+        LEFT = "L"
+        CENTER = "C"
+        RIGHT = "R"
+        JUSTIFIED = "J"
 
       def self.valid_justification?(justification)
-        [LEFT, CENTER, RIGHT, JUSTIFIED].include?(justification)
+        [LEFT, CENTER, RIGHT, JUSTIFIED].include? justification
       end
 
       def self.validate_justification(justification)
         raise InvalidJustificationError unless valid_justification?(justification)
       end
     end
-
   end
 end
